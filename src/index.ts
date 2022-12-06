37e67cce6fe03cdd7948a1885aa749457bcbcc6b
Oleksandr Kovalov <oleksandr.kovalov@gmail.com>
2022-12-06 10:39:44 -0500

    feat: fo-1359: add interfaces export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export * as utxoHelper from './services/utxoHelper';
+export {
+  AddUtxoItem,
+  UtxoOutputItem,
+  LedgerUtxoItem,
+  UtxoInputsInfo,
+  UtxoInputParameter,
+} from './services/utxoHelper';
 export * as builder from './services/builder';
 export * as fee from './services/fee';
